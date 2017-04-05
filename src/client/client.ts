43e0f30ea41c9d88304b1b74ab79c685eae4d4c0
Ian Henderson <ian.henderson@mlb.com>
2017-04-04 18:23:54 -0700

    updtes

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,21 @@
 import { name, age } from './ian'
 import { BaseData, Entity } from '../shared/shared'
+import { PC_COMMANDS, COMMAND_TYPES } from '../shared/config'
 
 
 export function init(): void {
 	console.log('hi', { name, age })
+}
+
+function commander(arg: PC_COMMANDS): COMMAND_TYPES {
+	switch (arg) {
+		case PC_COMMANDS.SHOW_PITCHES:
+		case PC_COMMANDS.ARSENAL:
+		case PC_COMMANDS.CLEAR_RENDER_QUEUE:
+		case PC_COMMANDS.CLEAR_PITCHES: {
+			return 23;
+			break
+		}
+		return 34
+	}
 }
